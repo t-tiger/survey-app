7edd9f5adae66fcef450f8748611893a180b4a01
Taiga Tachibana <tachiba@gmail.com>
2021-01-02 21:44:36 +0900

    add init.sql

---
+++ server/sql/init.sql
@@ -0,0 +1,13 @@
+create database survey;
+\c survey;
+
+create extension if not exists "uuid-ossp";
+
+create table users
+(
+    id              uuid default uuid_generate_v4() not null,
+    email           varchar(100)                    not null,
+    password_digest varchar                         not null,
+    created_at      timestamp                       not null,
+    updated_at      timestamp                       not null
+);
