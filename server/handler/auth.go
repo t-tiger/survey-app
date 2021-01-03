3ce16899436e91a2ccf6ac9251d442e6048b4dba
Taiga Tachibana <tachiba@gmail.com>
2021-01-03 14:27:45 +0900

    separate auth related functions

---
+++ server/handler/auth.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/t-tiger/survey/server/cerrors"
+	"github.com/t-tiger/survey/server/config"
+)
+
+const ctxUserID = "userID"
+
+// createToken generates json web token
+func createToken(userID string) (string, error) {
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	})
+	s, err := t.SignedString([]byte(config.Config.SecretKey))
+	if err != nil {
+		return "", cerrors.Errorf(cerrors.Unexpected, err.Error())
+	}
+	return s, nil
+}
+
+// retrieveUserID returns userID by decoding jwt with Authorization value
+func retrieveUserID(r *http.Request) *string {
+	authHead := r.Header.Get("Authorization")
+	if len(authHead) == 0 {
+		return nil
+	}
+	tokenStr := strings.Replace(authHead, "Bearer ", "", 1)
+
+	// decode json web token
+	c := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(tokenStr, c, func(t *jwt.Token) (interface{}, error) {
+		return []byte(config.Config.SecretKey), nil
+	})
+	if err != nil {
+		return nil
+	}
+	if v, ok := c["user_id"].(string); ok {
+		return &v
+	}
+	return nil
+}
+
+func AuthUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userID := retrieveUserID(r)
+		if userID == nil {
+			handleError(cerrors.Errorf(cerrors.Unauthorized, "unauthorized user"), w)
+			return
+		}
+		// set authorized user's id in context
+		ctx := context.WithValue(r.Context(), ctxUserID, *userID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
