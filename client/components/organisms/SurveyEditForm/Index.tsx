6a2796829de7f506e723c05c920e43d6b77d0082
Taiga Tachibana <tachiba@gmail.com>
2021-01-06 13:17:32 +0900

    strip options with empty title

--- client/components/organisms/SurveyEditForm/Index.tsx
+++ client/components/organisms/SurveyEditForm/Index.tsx
@@ -2,17 +2,13 @@ import React, { useEffect, useState } from 'react'
 
 import { Box, Button } from '@material-ui/core'
 
-import {
-  Option,
-  Question,
-  Survey,
-} from 'modules/survey/types'
+import { Option, Question, Survey } from 'modules/survey/types'
 import { issueId } from 'utils/id'
-import { validateSurvey } from "modules/survey/helpers";
+import { validateSurvey } from 'modules/survey/helpers'
 
 import QuestionEdit from 'components/organisms/SurveyEditForm/QuestionEdit'
 import MultiLineToolTip from 'components/atoms/MultiLineTooltip'
-import SurveyEdit from "components/organisms/SurveyEditForm/SurveyEdit";
+import SurveyEdit from 'components/organisms/SurveyEditForm/SurveyEdit'
 
 export type SurveyForEdit = {
   title: Survey['title']
@@ -30,7 +26,12 @@ type Props = {
   onSubmit: (survey: SurveyForEdit) => Promise<void>
 }
 
-const SurveyEditForm: React.FC<Props> = ({ survey, pageTitle, submitTitle, onSubmit }) => {
+const SurveyEditForm: React.FC<Props> = ({
+  survey,
+  pageTitle,
+  submitTitle,
+  onSubmit,
+}) => {
   const [sending, setSending] = useState(false)
   const [title, setTitle] = useState(survey.title || '')
   const [questions, setQuestions] = useState<SurveyForEdit['questions']>([
@@ -63,13 +64,23 @@ const SurveyEditForm: React.FC<Props> = ({ survey, pageTitle, submitTitle, onSub
   }
   const handleSubmit = async () => {
     setSending(true)
-    await onSubmit({ title, questions })
+    await onSubmit({
+      title,
+      questions: questions.map((q) => ({
+        ...q,
+        options: q.options.filter((o) => o.title.trim().length > 0),
+      })),
+    })
     setSending(false)
   }
 
   return (
     <>
-      <SurveyEdit pageTitle={pageTitle}  surveyTitle={title} onChange={setTitle} />
+      <SurveyEdit
+        pageTitle={pageTitle}
+        surveyTitle={title}
+        onChange={setTitle}
+      />
       {questions.map((q, i) => (
         <Box mt={3} key={q.id}>
           <QuestionEdit
